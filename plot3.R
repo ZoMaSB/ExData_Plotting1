98edaac4510fa9dafb7850c6d88830c59a5a3f0b
ZoMaSB <zoran.maricic@gmail.com>
2015-12-30 18:38:07 -0800

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,23 +1,32 @@
-library(ggplot2)
-# Download and unzip file to wd
-fileURL="https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
-download.file(fileURL, "EDAproject2.zip", method="curl")
-unzip("EDAproject2.zip", exdir="/Users/zm")
 
-# Read data
-NEI<-readRDS("summarySCC_PM25.rds")
-SCC<-readRDS("Source_Classification_Code.rds")
+# check if package is installed and launch library
+if(!require(data.table)){
+  install.packages("data.table")
+  library(data.table)
+}
+# Download file if not already present in wd
+fileURL="https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+destfile="./EDAPowerConsumptionProjectI.zip" 
+if (!file.exists(destfile)) {
+  download.file(fileURL ,destfile,method="curl") }
 
-#Baltimore City only subset
-subBalt<-subset(NEI, NEI$fips=='24510')
+# unzip file if not present in wd
+if (!file.exists("./household_power_consumption.txt")){
+  unzip("EDAPowerConsumptionProjectI.zip")
+}
+#load file into temp connection
+temp<-file("household_power_consumption.txt")
+open(temp)
+#read specific rows
+cleandata<-read.table("household_power_consumption.txt", sep=";", skip=66637, nrow=2880, col.names=colnames(read.table("household_power_consumption.txt", sep=";",header=TRUE)))
+#combine Data and Time 
+cleandata$DatePlusTime <- strptime(paste(cleandata$Date, cleandata$Time, sep=" "), 
+                                   format="%d/%m/%Y %H:%M:%S")
+#plot and create an image file
+png("plot3.png",width = 480, height = 480, units = "px")
+plot(cleandata$DatePlusTime, cleandata$Sub_metering_1, ylab="Energy sub metering", type="l", col="black",xlab="")
+lines(cleandata$DatePlusTime,cleandata$Sub_metering_2, type="l", col="red")
+lines(cleandata$DatePlusTime,cleandata$Sub_metering_3, type="l", col="blue")
+legend("topright", col=c("black","red","blue"),lty=1, legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 
-# Data plot by 'type' and png image creation
-png(filename="plot3.png", width = 480, height=480, units="px")
-ggplot(data=subBalt, aes(factor(year), Emissions, fill=type))+
-  geom_bar(stat="identity")+
-  theme_bw()+guides(fill=FALSE)+
-  facet_grid(.~type, scales = "free", space="free")+
-  labs(x="Year", y="Total PM2.5 Emission in Tons")+
-  labs(title="PM2.5 Emission in Tons for Baltimore City (1999-2008)")
-
-dev.off()
+dev.off()                      
